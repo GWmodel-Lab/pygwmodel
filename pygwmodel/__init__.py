f9e9bf1cd7d21c53585bfb878271d34ca7297827
HPDell <huyg0180110559@outlook.com>
2023-07-28 14:14:43 +0100

    edit: rename high_api to api

--- pygwmodel/__init__.py
+++ pygwmodel/__init__.py
@@ -1,5 +1,5 @@
-from .high_api import KernelType, BandwidthSelectionCriterionType
-from .high_api import GWRBasic
+from .api import KernelType, BandwidthSelectionCriterionType
+from .api import GWRBasic
 
 if __name__ == "__main__":
     print("PyGWmodel Package.")
