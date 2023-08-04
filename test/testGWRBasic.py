444934400dfc689fc97f2e84784e2b3d8516abaf
HPDell <huyg0180110559@outlook.com>
2023-08-04 07:00:00 +0000

    edit: reopen tests for GWRBasic

--- test/testGWRBasic.py
+++ test/testGWRBasic.py
@@ -40,56 +40,58 @@ class TestGWRBasic(unittest.TestCase):
                 ])
                 self.assertTrue(np.all(np.abs(diagnostic0 - diagnostic) < 1e-8))
 
-    # def test_autoselect_bandwidth(self):
-    #     for p in self.parallel_case:
-    #         with self.subTest(parallel=p):
-    #             algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_bw=pygwmodel.BandwidthSelectionCriterionType.CV, multithreads=p)
-    #             self.assertEqual(algorithm.bw, 67)
+    def test_autoselect_bandwidth(self):
+        for p in self.parallel_case:
+            with self.subTest(parallel=p):
+                algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_bw=pygwmodel.BandwidthSelectionCriterionType.CV, multithreads=p)
+                self.assertEqual(algorithm.bw, 67)
 
-    # def test_autoselect_indepvars(self):
-    #     for p in self.parallel_case:
-    #         with self.subTest(parallel=p):
-    #             algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_var=3.0, multithreads=p)
-    #             criterion = algorithm.indep_var_select_criterions
-    #             self.assertSequenceEqual(criterion[0][0], ['UNEMPLOY'])
-    #             self.assertSequenceEqual(criterion[1][0], ['PROF'])
-    #             self.assertSequenceEqual(criterion[2][0], ['FLOORSZ'])
-    #             self.assertSequenceEqual(criterion[3][0], ['FLOORSZ', 'PROF'])
-    #             self.assertSequenceEqual(criterion[4][0], ['FLOORSZ', 'UNEMPLOY'])
-    #             self.assertSequenceEqual(criterion[5][0], ['FLOORSZ', 'UNEMPLOY', 'PROF'])
-    #             self.assertAlmostEqual(criterion[0][1], 2551.61359020599, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[1][1], 2551.30032201349, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[2][1], 2468.93236280013, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[3][1], 2452.86447942033, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[4][1], 2450.59642666509, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[5][1], 2452.80388934625, delta=1e-8)
-    #             self.assertSequenceEqual(algorithm.indep_vars, ['FLOORSZ', 'PROF'])
+    def test_autoselect_indepvars(self):
+        for p in self.parallel_case:
+            with self.subTest(parallel=p):
+                algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_var=3.0, multithreads=p)
+                criterion = algorithm.indep_var_select_criterions
+                self.assertSequenceEqual(criterion[0][0], ['UNEMPLOY'])
+                self.assertSequenceEqual(criterion[1][0], ['PROF'])
+                self.assertSequenceEqual(criterion[2][0], ['FLOORSZ'])
+                self.assertSequenceEqual(criterion[3][0], ['FLOORSZ', 'PROF'])
+                self.assertSequenceEqual(criterion[4][0], ['FLOORSZ', 'UNEMPLOY'])
+                self.assertSequenceEqual(criterion[5][0], ['FLOORSZ', 'UNEMPLOY', 'PROF'])
+                self.assertAlmostEqual(criterion[0][1], 2551.61359020599, delta=1e-8)
+                self.assertAlmostEqual(criterion[1][1], 2551.30032201349, delta=1e-8)
+                self.assertAlmostEqual(criterion[2][1], 2468.93236280013, delta=1e-8)
+                self.assertAlmostEqual(criterion[3][1], 2452.86447942033, delta=1e-8)
+                self.assertAlmostEqual(criterion[4][1], 2450.59642666509, delta=1e-8)
+                self.assertAlmostEqual(criterion[5][1], 2452.80388934625, delta=1e-8)
+                self.assertSequenceEqual(algorithm.indep_vars, ['FLOORSZ', 'PROF'])
 
-    # def test_autoselect_all(self):
-    #     for p in self.parallel_case:
-    #         with self.subTest(parallel=p):
-    #             algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_var=3.0, optimize_bw=pygwmodel.BandwidthSelectionCriterionType.CV, multithreads=p)
-    #             criterion = algorithm.indep_var_select_criterions
-    #             self.assertSequenceEqual(criterion[0][0], ['UNEMPLOY'])
-    #             self.assertSequenceEqual(criterion[1][0], ['PROF'])
-    #             self.assertSequenceEqual(criterion[2][0], ['FLOORSZ'])
-    #             self.assertSequenceEqual(criterion[3][0], ['FLOORSZ', 'PROF'])
-    #             self.assertSequenceEqual(criterion[4][0], ['FLOORSZ', 'UNEMPLOY'])
-    #             self.assertSequenceEqual(criterion[5][0], ['FLOORSZ', 'UNEMPLOY', 'PROF'])
-    #             self.assertAlmostEqual(criterion[0][1], 2551.61359020599, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[1][1], 2551.30032201349, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[2][1], 2468.93236280013, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[3][1], 2452.86447942033, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[4][1], 2450.59642666509, delta=1e-8)
-    #             self.assertAlmostEqual(criterion[5][1], 2452.80388934625, delta=1e-8)
-    #             self.assertSequenceEqual(algorithm.indep_vars, ['FLOORSZ', 'PROF'])
-    #             self.assertEqual(algorithm.bw, 31)
+    def test_autoselect_all(self):
+        for p in self.parallel_case:
+            with self.subTest(parallel=p):
+                algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(optimize_var=3.0, optimize_bw=pygwmodel.BandwidthSelectionCriterionType.CV, multithreads=p)
+                criterion = algorithm.indep_var_select_criterions
+                self.assertSequenceEqual(criterion[0][0], ['UNEMPLOY'])
+                self.assertSequenceEqual(criterion[1][0], ['PROF'])
+                self.assertSequenceEqual(criterion[2][0], ['FLOORSZ'])
+                self.assertSequenceEqual(criterion[3][0], ['FLOORSZ', 'PROF'])
+                self.assertSequenceEqual(criterion[4][0], ['FLOORSZ', 'UNEMPLOY'])
+                self.assertSequenceEqual(criterion[5][0], ['FLOORSZ', 'UNEMPLOY', 'PROF'])
+                self.assertAlmostEqual(criterion[0][1], 2551.61359020599, delta=1e-8)
+                self.assertAlmostEqual(criterion[1][1], 2551.30032201349, delta=1e-8)
+                self.assertAlmostEqual(criterion[2][1], 2468.93236280013, delta=1e-8)
+                self.assertAlmostEqual(criterion[3][1], 2452.86447942033, delta=1e-8)
+                self.assertAlmostEqual(criterion[4][1], 2450.59642666509, delta=1e-8)
+                self.assertAlmostEqual(criterion[5][1], 2452.80388934625, delta=1e-8)
+                self.assertSequenceEqual(algorithm.indep_vars, ['FLOORSZ', 'PROF'])
+                self.assertEqual(algorithm.bw, 31)
     
     def test_predict(self):
-        algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit()
-        prediction = algorithm.predict(self.londonhp)
-        self.assertIn("y_hat", prediction.columns)
-        self.assertIn("residual", prediction.columns)
+        for p in self.parallel_case:
+            with self.subTest(parallel=p):
+                algorithm = GWRBasic(self.londonhp, self.depen, self.indep, 36.0, longlat=False).fit(multithreads=p)
+                prediction = algorithm.predict(self.londonhp, multithreads=p)
+                self.assertIn("y_hat", prediction.columns)
+                self.assertIn("residual", prediction.columns)
 
 
 if __name__ == '__main__':
