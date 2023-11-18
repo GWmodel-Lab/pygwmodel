66eea1764d1dc44f413862103a211e3c04af48c9
HPDell <huyg0180110559@outlook.com>
2023-11-18 17:21:38 +0000

    edit: move docs to /doc folder

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./src/pygwmodel'))
+sys.path.insert(0, os.path.abspath('../src/pygwmodel'))
 
 
 # -- Project information -----------------------------------------------------
--- doc/index.rst
+++ doc/index.rst
@@ -10,7 +10,7 @@ Welcome to pygwmodel's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-   doc/api/modules.rst
+   api/modules.rst
 
 Indices and tables
 ==================
