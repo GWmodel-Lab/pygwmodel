7b65f87e2b33cf378346477bfd077cf59d6ad837
HPDell <huyg0180110559@outlook.com>
2023-11-18 17:55:59 +0000

    version: 0.1.0

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, GWmodel-Lab'
 author = 'GWmodel-Lab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
